75120cd89c28b2f416c314057b5a2d6d074b3bce
iqboljonshuhratov <shuhratjonoviqboljon79@gmail.com>
2023-05-27 18:05:50 +0500

    loyha

--- src/components/boshSahifa/index.js
+++ src/components/boshSahifa/index.js
@@ -98,7 +98,7 @@ export default function BoshSaxifa() {
         } fs-1 text-center mb-4`}
       >
         {" "}
-        Frontend developer{" "}
+        Frontend developerr{" "}
       </p>
 
       <div class="ijtimoiyTarmoqlar">
